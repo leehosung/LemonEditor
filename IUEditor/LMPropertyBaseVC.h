d2b3f75adc4716bc8d67316c4877c6eb6e3f7e1e
JDYang <j@jdlab.org>
2014-04-06 02:08:48 +0900

    add default log

---
+++ IUEditor/LMPropertyBaseVC.h
@@ -0,0 +1,13 @@
+//
+//  LMPropertyBaseVC.h
+//  IUEditor
+//
+//  Created by JD on 4/5/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface LMPropertyBaseVC : NSViewController
+
+@end
---
+++ IUEditor/LMPropertyFrameVC.m
@@ -0,0 +1,32 @@
+//
+//  LMPropertyVC.m
+//  IUEditor
+//
+//  Created by jd on 4/3/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import "LMPropertyVC.h"
+#import "IUObj.h"
+#import "IUCSS.h"
+
+@interface LMPropertyVC ()
+@property (weak) IBOutlet NSTextField *wTF;
+@property (weak) IBOutlet NSTextField *hTF;
+
+@end
+
+@implementation LMPropertyVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+
+
+
+@end
