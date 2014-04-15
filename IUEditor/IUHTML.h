5a7bece6d556e5aff5696fb13abeaf38ef8f64d1
SeungmiChoi <s@jdlab.org>
2014-04-15 18:02:29 +0900

    update : base property view

---
+++ IUEditor/IUHTML.h
@@ -0,0 +1,13 @@
+//
+//  IUHTML.h
+//  IUEditor
+//
+//  Created by ChoiSeungmi on 2014. 4. 15..
+//  Copyright (c) 2014년 JDLab. All rights reserved.
+//
+
+#import "IUBox.h"
+
+@interface IUHTML : IUBox
+
+@end
