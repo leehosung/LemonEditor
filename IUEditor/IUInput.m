606f2e9659ae3eb37395a450faea2d32afc6bbe8
SeungmiChoi <s@jdlab.org>
2014-04-21 14:17:13 +0900

    add IUInput, modify : image size in stackVC

---
+++ IUEditor/IUInput.m
@@ -0,0 +1,13 @@
+//
+//  IUInput.m
+//  IUEditor
+//
+//  Created by ChoiSeungme on 2014. 4. 21..
+//  Copyright (c) 2014년 JDLab. All rights reserved.
+//
+
+#import "IUInput.h"
+
+@implementation IUInput
+
+@end
