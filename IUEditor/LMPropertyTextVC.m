873a1d2ba7ee58156dccac56e1ec948e3e17009b
jiwon <w@jdlab.org>
2014-04-24 14:59:14 +0900

    text Type 설정

--- IUEditor/LMPropertyTextVC.m
+++ IUEditor/LMPropertyTextVC.m
@@ -10,6 +10,15 @@
 
 @interface LMPropertyTextVC ()
 
+@property (weak) IBOutlet NSComboBox *fontB;
+@property (weak) IBOutlet NSTextField *fontSizeB;
+@property (weak) IBOutlet NSColorWell *fontColorWell;
+@property NSArray *fonts;
+
+@property (weak) IBOutlet NSSegmentedControl *fontStyleB;
+@property (weak) IBOutlet NSSegmentedControl *textAlignB;
+@property (weak) IBOutlet NSComboBox *lineHeightB;
+
 @end
 
 @implementation LMPropertyTextVC
@@ -18,18 +27,52 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
+        self.fonts = @[@"Georgia", @"Times New Roman", @"Arial" ];
     }
     return self;
 }
 
-- (void)setController:(IUController *)controller{
-    assert(_controller == nil); // should called only once
-    _controller = controller;
-    [_controller addObserver:self forKeyPath:@"selectedTextRange" options:0 context:nil];
+- (NSString*)CSSBindingPath:(IUCSSTag)tag{
+    return [@"controller.selection.css.assembledTagDictionary." stringByAppendingString:tag];
 }
 
-- (void)selectedTextRangeDidChange:(NSDictionary*)change{
+- (IBAction)fontDecoBPressed:(id)sender {
+
+    BOOL value;
+    value = [sender isSelectedForSegment:0];
+    [self setValue:@(value) forKeyPath:[self CSSBindingPath:IUCSSTagFontWeight]];
+    
+    value = [sender isSelectedForSegment:1];
+    [self setValue:@(value) forKeyPath:[self CSSBindingPath:IUCSSTagFontStyle]];
+    
+    value = [sender isSelectedForSegment:2];
+    [self setValue:@(value) forKeyPath:[self CSSBindingPath:IUCSSTagTextDecoration]];
     
 }
+
+- (IBAction)textAligneBPressed:(id)sender {
+    
+    NSString *textalign;
+    if([sender isSelectedForSegment:0]){
+        textalign= @"left";    }
+    if([sender isSelectedForSegment:1]){
+        textalign=@"center";    }
+    if([sender isSelectedForSegment:2]){
+        textalign=@"right";    }
+    if([sender isSelectedForSegment:3]){
+        textalign= @"stretch";    }
+    
+    [self setValue:textalign forKeyPath:[self CSSBindingPath:IUCSSTagTextAlign]];
+}
+
+
+-(void)awakeFromNib{
+    
+    [_fontB bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagFontName] options:nil];
+    [_fontB bind:@"contentValues" toObject:self withKeyPath:@"fonts" options:nil];
+    [_fontSizeB bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagFontSize] options:nil];
+    [_fontColorWell bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagFontColor] options:nil];
+    [_lineHeightB bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagLineHeight] options:nil];
+ }
 
 @end
