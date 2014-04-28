4ae5cfa604c5d21bace9a7bfb46bbd5447a1baf4
SeungmiChoi <s@jdlab.org>
2014-04-28 14:04:33 +0900

    Update : IUEvent Visible

--- IUEditor/LMPropertyVisibleVC.m
+++ IUEditor/LMPropertyVisibleVC.m
@@ -9,6 +9,10 @@
 #import "LMPropertyVisibleVC.h"
 
 @interface LMPropertyVisibleVC ()
+@property (weak) IBOutlet NSTextField *equationTF;
+@property (weak) IBOutlet NSTextField *durationTF;
+@property (weak) IBOutlet NSStepper *durationStepper;
+@property (weak) IBOutlet NSMatrix *directionMatrix;
 
 @end
 
@@ -21,6 +25,18 @@
         // Initialization code here.
     }
     return self;
+}
+
+- (void)awakeFromNib{
+    
+    [_equationTF bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToEventTag:IUEventTagVisibleEquation] options:IUBindingDictNotRaisesApplicable];
+    [_durationTF bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToEventTag:IUEventTagVisibleDuration] options:IUBindingDictNotRaisesApplicable];
+    [_durationStepper bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToEventTag:IUEventTagVisibleDuration] options:IUBindingDictNotRaisesApplicable];
+    
+    [_directionMatrix bind:NSSelectedIndexBinding toObject:self withKeyPath:[_controller keyPathFromControllerToEventTag:IUEventTagVisibleDirection] options:IUBindingDictNotRaisesApplicable];
+    
+    
+
 }
 
 
