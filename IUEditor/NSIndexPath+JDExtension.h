a507751e0e4b74f8e1cb74afbdc95709c5c4391d
JD Yang <j@jdlab.org>
2014-05-03 17:18:52 +0900

    project에서 js컴파일 삭제

---
+++ IUEditor/NSIndexPath+JDExtension.h
@@ -0,0 +1,13 @@
+//
+//  NSIndexPath+JDExtension.h
+//  IUEditor
+//
+//  Created by jd on 5/3/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSIndexPath (JDExtension)
+
+@end
