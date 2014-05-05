796a28b1c9f3ba0cf1b6dd73dcf87ea4475bba56
SeungmiChoi <s@jdlab.org>
2014-05-05 17:24:51 +0900

    update iutextfield

--- IUEditor/IUTextField.m
+++ IUEditor/IUTextField.m
@@ -10,8 +10,63 @@
 
 @implementation IUTextField
 
+
+-(id)initWithManager:(IUIdentifierManager *)manager option:(NSDictionary *)option{
+    assert(manager!=nil);
+    self = [super initWithManager:manager option:option];
+    if(self){
+        _placeholder = @"placeholder";
+        _inputValue = @"value example";
+        _type = IUTextFieldTypeDefault;
+        
+        [self.css setValue:@(130) forTag:IUCSSTagWidth forWidth:IUCSSMaxViewPortWidth];
+        [self.css setValue:@(30) forTag:IUCSSTagHeight forWidth:IUCSSMaxViewPortWidth];
+
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder{
+    self =  [super initWithCoder:aDecoder];
+    if(self){
+        [aDecoder decodeToObject:self withProperties:[[IUTextField class] properties]];
+    }
+    return self;
+}
+-(void)encodeWithCoder:(NSCoder *)aCoder{
+    [super encodeWithCoder:aCoder];
+    [aCoder encodeFromObject:self withProperties:[[IUTextField class] properties]];
+    
+}
+
+#pragma mark -
+#pragma mark should
+
 -(BOOL)shouldEditText{
     return NO;
+}
+
+#pragma mark -
+#pragma mark setting
+
+
+- (void)setFormName:(NSString *)formName{
+    _formName = formName;
+    [self.delegate IU:self.htmlID HTML:self.html withParentID:self.htmlID];
+}
+
+- (void)setPlaceholder:(NSString *)placeholder{
+    _placeholder = placeholder;
+    [self.delegate IU:self.htmlID HTML:self.html withParentID:self.htmlID];
+}
+
+- (void)setInputValue:(NSString *)inputValue{
+    _inputValue = inputValue;
+    [self.delegate IU:self.htmlID HTML:self.html withParentID:self.htmlID];
+}
+- (void)setType:(IUTextFieldType)type{
+    _type = type;
+    [self.delegate IU:self.htmlID HTML:self.html withParentID:self.htmlID];
 }
 
 @end
