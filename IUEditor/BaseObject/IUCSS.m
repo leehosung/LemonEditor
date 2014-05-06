7ae1c46be2c117a55141c8f71d849436dc6173d9
SeungmiChoi <s@jdlab.org>
2014-05-06 19:14:37 +0900

    bugfix : border

--- IUEditor/BaseObject/IUCSS.m
+++ IUEditor/BaseObject/IUCSS.m
@@ -139,60 +139,56 @@
     }
 }
 
+- (BOOL)isSameBorder{
+    int borderTop = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderTopWidth] intValue];
+    int borderBottom = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderBottomWidth] intValue];
+    int borderLeft = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderLeftWidth] intValue];
+    int borderRight = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderRightWidth] intValue];
+    
+    if(borderTop == borderBottom == borderLeft == borderRight){
+        return YES;
+    }
+    return NO;
+}
+- (BOOL)isSameRadius{
+    int borderTLRadius = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderRadiusTopLeft] intValue];
+    int borderTRRadius = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderRadiusTopRight] intValue];
+    int borderBLRadius = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderRadiusBottomLeft] intValue];
+    int borderBRRadius = [[self.assembledTagDictionary objectForKey:IUCSSTagBorderRadiusBottomRight] intValue];
+    
+    if(borderTLRadius == borderTRRadius == borderBLRadius == borderBRRadius){
+        return YES;
+    }
+    return NO;
+}
+
 -(id)valueForKeyPath:(NSString *)keyPath{
     if ([keyPath containsString:@"assembledTagDictionary."]) {
         NSString *tag = [keyPath substringFromIndex:23];
         if ([tag isSameTag:IUCSSTagBorderWidth]) {
-            NSNumber* value = [self.assembledTagDictionary objectForKey:IUCSSTagBorderTopWidth];
-            
-            if ([value isEqualToNumber:[self.assembledTagDictionary objectForKey:IUCSSTagBorderLeftWidth]] == NO) {
+            NSNumber* value = [self.assembledTagDictionary objectForKey:IUCSSTagBorderWidth];
+            if(value == nil){
+                return @(0);
+            }
+            if([self isSameBorder]){
+                return value;
+            }
+            else{
                 return NSMultipleValuesMarker;
             }
-            
-            if ([value isEqualToNumber:[self.assembledTagDictionary objectForKey:IUCSSTagBorderRightWidth]] == NO) {
-                return NSMultipleValuesMarker;
-            }
-            
-            if ([value isEqualToNumber:[self.assembledTagDictionary objectForKey:IUCSSTagBorderBottomWidth]] == NO) {
-                return NSMultipleValuesMarker;
-            }
-            
-            return value;
-        }
-        //color multiple
-        if ([tag isSameTag:IUCSSTagBorderColor]){
-   
-            NSColor *topColor = [self.assembledTagDictionary objectForKey:IUCSSTagBorderTopColor];
-            NSString *colorString = [topColor rgbaString];
-            
-            if ([colorString isEqualToString:[[self.assembledTagDictionary objectForKey:IUCSSTagBorderLeftColor] rgbaString]] == NO) {
-                return NSMultipleValuesMarker;
-            }
-            if ([colorString isEqualToString:[[self.assembledTagDictionary objectForKey:IUCSSTagBorderRightColor] rgbaString]] == NO) {
-                return NSMultipleValuesMarker;
-            }
-            if ([colorString isEqualToString:[[self.assembledTagDictionary objectForKey:IUCSSTagBorderBottomColor] rgbaString]] == NO) {
-                return NSMultipleValuesMarker;
-            }
-            return topColor;
         }
         //radius multiple
         if ([tag isSameTag:IUCSSTagBorderRadius]) {
-            NSInteger borderTLRadius = [self.assembledTagDictionary[IUCSSTagBorderRadiusTopLeft] integerValue];
-            NSInteger borderTRRadius = [self.assembledTagDictionary[IUCSSTagBorderRadiusTopRight] integerValue];
-            NSInteger borderBLRadius = [self.assembledTagDictionary[IUCSSTagBorderRadiusBottomLeft] integerValue];
-            NSInteger borderBRRadius = [self.assembledTagDictionary[IUCSSTagBorderRadiusBottomRight] integerValue];
-            
-            if (borderTLRadius == 0 && borderTRRadius == 0 \
-                && borderBLRadius == 0 && borderBRRadius == 0) {
+            NSNumber* value = [self.assembledTagDictionary objectForKey:IUCSSTagBorderRadius];
+            if(value == nil){
                 return @(0);
             }
-            
-            if (borderTLRadius == borderTRRadius && borderTLRadius == borderBLRadius
-                && borderTLRadius == borderBRRadius) {
-                return @(borderTLRadius);
+            if([self isSameRadius]){
+                return value;
             }
-            return NSMultipleValuesMarker;
+            else{
+                return NSMultipleValuesMarker;
+            }
         }
         return [_assembledTagDictionaryForEditWidth objectForKey:tag];
     }
