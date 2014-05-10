a27d685acd3c9b604e837014feb690de23ca8770
JD Yang <j@jdlab.org>
2014-05-10 22:45:14 +0900

    page link set 추가

---
+++ IUEditor/LMPropertyIUPageLinkSetVC.h
@@ -0,0 +1,15 @@
+//
+//  IUPageLinkSetVC.h
+//  IUEditor
+//
+//  Created by jd on 5/8/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "IUController.h"
+@interface LMPropertyIUPageLinkSetVC : NSViewController
+
+@property (nonatomic) IUController      *controller;
+
+@end
