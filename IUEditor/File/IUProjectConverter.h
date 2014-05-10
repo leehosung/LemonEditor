655b141f49f59810093de553efeb5f9b97a9e8cd
JD Yang <j@jdlab.org>
2014-05-10 22:51:26 +0900

    pj converter 추가

---
+++ IUEditor/File/IUProjectConverter.h
@@ -0,0 +1,13 @@
+//
+//  IUProjectConverter.h
+//  IUEditor
+//
+//  Created by jd on 5/10/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IUProjectConverter : NSObject
+
+@end
---
+++ IUEditor/File/IUProjectConverter.m
@@ -0,0 +1,13 @@
+//
+//  IUProjectConverter.m
+//  IUEditor
+//
+//  Created by jd on 5/10/14.
+//  Copyright (c) 2014 JDLab. All rights reserved.
+//
+
+#import "IUProjectConverter.h"
+
+@implementation IUProjectConverter
+
+@end
