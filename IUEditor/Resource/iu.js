f2560585a3d8af1b5aa262a40ed6c8d3e7072a4e
SeungmiChoi <s@jdlab.org>
2014-06-02 14:06:51 +0900

    bugfix : javascript initialize (racing condition) Conflicts: 	IUEditor/File/IUProject.m

--- IUEditor/Resource/iu.js
+++ IUEditor/Resource/iu.js
@@ -61,39 +61,7 @@ function flyFromRight(eventObject){
 }
 
 $(document).ready(function(){
-    if (typeof isEditor != 'undefined' && isEditor == true){
-        return;
-    }
-    console.log('start : transition code');
-     
-    //Initialize transition
-	$('.IUTransition').each(function(){
-		var eventType = $(this).attr('transitionevent');
-		var transitionanimation = $(this).attr('transitionanimation');
-		
-		if (eventType=='mouseOn'){
-			$(this).bind('mouseenter',window[transitionanimation]);
-			$(this).bind('mouseleave',window[transitionanimation]);
-		}
-		else {
-            $(this).bind(eventType, window[transitionanimation]);
-		}
-                            if (transitionanimation == 'flyFromRight'){
-                            $($(this).children()[1]).css('left', '100%');
-                            }
-    });
-
-    //move : current viewport pc type
-	if(isMobile()==false){
-	    $('[xPosMove]').each(function(){
-			var xPosMove = $(this).attr('xPosMove');
-			if ($(this).css('position') == 'absolute'){
-				var startLeft = parseFloat($(this).css('left')) - xPosMove;
-			    $(this).css('left', startLeft + 'px');
-				$(this).attr('startLeft', startLeft);
-			};
-		});
-	}
+    console.log('iu.js')
 });
 
 function onYouTubePlayerReady(playerId){
