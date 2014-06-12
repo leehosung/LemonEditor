1fd5c39b9ae92a44af218d5587a8e861f3f6b9bc
Geunmin.Moon <GeunminMoon@GeunminMoonui-MacBook-Pro.local>
2014-06-12 14:55:07 +0900

    Set a default value of submit button as 'Submit'

--- IUEditor/PGSubmitButton.m
+++ IUEditor/PGSubmitButton.m
@@ -10,10 +10,12 @@
 
 @implementation PGSubmitButton
 
-- (void)setLabel:(NSString *)label{
-    _label = label;
-    [self.delegate IUHTMLIdentifier:self.htmlID HTML:self.html withParentID:self.parent.htmlID];
-    NSLog(@"SUCCESS!!!!");
+-(id)initWithProject:(IUProject *)project options:(NSDictionary *)options{
+    self = [super initWithProject:project options:options];
+    if (self) {
+        self.label = @"Submit"; //place holder 초기값은 Submit
+    }
+    return self;
 }
 
 -(id)initWithCoder:(NSCoder *)aDecoder{
@@ -26,5 +28,16 @@
     [super encodeWithCoder:aCoder];
     [aCoder encodeFromObject:self withProperties:[PGSubmitButton properties]];
 }
+
+
+- (void)setLabel:(NSString *)label{
+    if (label == nil)
+        _label = @"";   //place holder 에 빈칸 입력시 null 로 표시되는 것을 방지
+    else
+        _label = label;
+    
+    [self.delegate IUHTMLIdentifier:self.htmlID HTML:self.html withParentID:self.parent.htmlID];
+}
+
 
 @end
