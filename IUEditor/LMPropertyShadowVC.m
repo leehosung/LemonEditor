0ed1ffc48f053bfa47c171a887f0bc086290f11a
SeungmiChoi <s@jdlab.org>
2014-06-16 18:58:44 +0900

    update : round shadow value

--- IUEditor/LMPropertyShadowVC.m
+++ IUEditor/LMPropertyShadowVC.m
@@ -14,6 +14,7 @@
 @property (weak) IBOutlet NSSlider *shadowH;
 @property (weak) IBOutlet NSSlider *shadowSprd;
 @property (weak) IBOutlet NSSlider *shadowBlr;
+
 @property (weak) IBOutlet NSTextField *shadowVText;
 @property (weak) IBOutlet NSTextField *shadowHText;
 @property (weak) IBOutlet NSTextField *shadowSpreadText;
@@ -38,11 +39,23 @@
 }
 
 -(void)awakeFromNib{
+
     [_shadowColor bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowColor] options:IUBindingDictNotRaisesApplicable];
+    
     [_shadowV bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowVertical] options:IUBindingDictNotRaisesApplicable];
     [_shadowH bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowHorizontal] options:IUBindingDictNotRaisesApplicable];
     [_shadowSprd bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowSpread] options:IUBindingDictNotRaisesApplicable];
     [_shadowBlr bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowBlur] options:IUBindingDictNotRaisesApplicable];
+    
+    
+    NSNumberFormatter *formatter = [[NSNumberFormatter alloc] init];
+    [formatter setRoundingMode:NSNumberFormatterRoundDown];
+    [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
+    [formatter setMaximumFractionDigits:0];
+    [_shadowVText setFormatter:formatter];
+    [_shadowHText setFormatter:formatter];
+    [_shadowSpreadText setFormatter:formatter];
+    [_shadowBlurText setFormatter:formatter];
 
     [_shadowVText bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowVertical] options:@{NSNullPlaceholderBindingOption: @(0)}];
     [_shadowHText bind:@"value" toObject:self withKeyPath:[self CSSBindingPath:IUCSSTagShadowHorizontal] options:@{NSNullPlaceholderBindingOption: @(0)}];
