acdfe512fe930a2ffaa9a0929de2ff965b0bd5fa
SeungmiChoi <s@jdlab.org>
2014-06-30 11:26:07 +0900

    update : bg gradient

--- IUEditor/LMPropertyBGColorVC.m
+++ IUEditor/LMPropertyBGColorVC.m
@@ -33,16 +33,33 @@
     [NSColor setIgnoresAlpha:NO];
 
     [_bgColorWell bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGColor] options:IUBindingDictNotRaisesApplicable];
+    
+    //gradient
+    [_enableGradientBtn bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradient] options:IUBindingDictNotRaisesApplicable];
+    
+    [_bgGradientStartColorWell bind:NSEnabledBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradient] options:IUBindingDictNotRaisesApplicable];
+    [_bgGradientEndColorWell bind:NSEnabledBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradient] options:IUBindingDictNotRaisesApplicable];
+    
     [_bgGradientStartColorWell bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradientStartColor] options:IUBindingDictNotRaisesApplicable];
     [_bgGradientEndColorWell bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradientEndColor] options:IUBindingDictNotRaisesApplicable];
-    
-    [_enableGradientBtn bind:NSValueBinding toObject:self withKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradient] options:IUBindingDictNotRaisesApplicable];
 
 
 }
 
 - (void)makeClearColor:(id)sender{
     [self setValue:nil forKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGColor]];
+}
+- (IBAction)clickEnableGradient:(id)sender {
+    if([_enableGradientBtn state]){
+        id currentColor = [self valueForKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGColor]];
+        [self setValue:currentColor forKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradientStartColor]];
+        [self setValue:currentColor forKeyPath:[_controller keyPathFromControllerToCSSTag:IUCSSTagBGGradientEndColor]];
+
+    }
+    else{
+        [_bgGradientStartColorWell deactivate];
+        [_bgGradientEndColorWell deactivate];
+    }
 }
 
 @end
